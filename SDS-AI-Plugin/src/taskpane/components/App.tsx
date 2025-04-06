f0b640e97412fb818e787de2fcf518e2f4d93967
smery <smey@sds-ch.com>
2025-04-06 16:42:08 +0200

    comitting 2

--- SDS-AI-Plugin/src/taskpane/components/App.tsx
+++ SDS-AI-Plugin/src/taskpane/components/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC<AppProps> = (props: AppProps) => {
   const listItems: HeroListItem[] = [
     {
       icon: <Ribbon24Regular />,
-      primaryText: "Achieve more with Office integration 222",
+      primaryText: "Achieve more with Office integration 2222",
     },
     {
       icon: <LockOpen24Regular />,
